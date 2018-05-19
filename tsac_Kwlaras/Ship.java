bfcc14234c179afee9fa7ffb148b18296f3f4230
iamtsac <39421505+iamtsac@users.noreply.github.com>
2018-05-19 21:43:42 +0300

    Update Ship.java

--- tsac_Kwlaras/Ship.java
+++ tsac_Kwlaras/Ship.java
@@ -1,1 +1,41 @@
-d
+import java.util.*;
+
+public abstract class Ship {
+
+  private Tile startCell;
+  private char orient;
+  private  int size;
+
+  startCell = new Tile(
+      
+  public void placeShip(Tile startCell, char or, Board b, boolean verbose){
+  
+    boolean verbose=false;
+
+
+   //Giving the orientation for ship's placement
+   
+    System.out.println("Please Give Orientation: ");
+
+    //Case that Orientation is Horizontal
+    if(or =="h") {
+      
+        for( int j=startCell; j<(startCell+size); j++){
+
+         b[startCell][j] = s; 
+
+        }
+
+    }
+    //Case that Orientation is Vertical
+     else if(or== "v" ){
+     
+       for(int i=startCell; i<(startCell + size ); j++){
+
+         b[i][startCell] = s ;
+       }
+     
+     }
+  }
+    
+}
