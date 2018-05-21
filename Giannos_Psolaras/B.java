c1438781ad19354f4136d02dbf5a03c8429bb821
cgiannos <37047743+cgiannos@users.noreply.github.com>
2018-05-21 23:03:31 +0300

    Add files via upload

---
+++ Giannos_Psolaras/B.java
@@ -0,0 +1,16 @@
+public class B{
+public static void main(String args[]){
+Board c=new Board();
+Ship p=new Ship();
+/*for(int i=0; i<10; i++)
+  for(int j=0; j<10; j++){
+System.out.println(c.board[i][j].getSymbol());
+}
+}*/
+c.drawboards();
+p.placeShip(3,3,'h',c,false);
+c.drawboards();
+
+
+}
+}
