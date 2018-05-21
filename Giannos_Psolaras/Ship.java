266240e47c0fdfbbbef99279a1bb40f5cc9487bb
cgiannos <37047743+cgiannos@users.noreply.github.com>
2018-05-22 00:51:25 +0300

    Update Ship.java

--- Giannos_Psolaras/Ship.java
+++ Giannos_Psolaras/Ship.java
@@ -1,46 +1,39 @@
-import java.util.*;
-
 public  class Ship {
 
-  private int startCellx;
-  private int startCelly;
-  private char orient;
-  private  int size=3;
+    private char orient;
+    private  int size=3;
 
 
-  public Ship(){}
-//  Tile startCell;
-     
-  public void placeShip( char or, Board b, boolean verbose){
-  
-Tile  startCell = new Tile (1 , 2 , Tile.TileType.SEA);
-    this.startCellx = startCell.getX(); 
-    this.startCelly = startCell.getY();
+    public void placeShip(Tile t,char or, Board b, boolean verbose){
 
-    verbose=false;
 
-   //Giving the orientation for ship's placement
-   
-    System.out.println("Please Give Orientation: ");
 
-    //Case that Orientation is Horizontal
-    if(or == 'h') {
-        for( int j=startCellx; j<size; j++){
-          
-			
-         b.board[startCelly][j].setTileType(Tile.TileType.SHIP);
+        verbose=false;
 
-			
+        //Giving the orientation for ship's placement
+
+        System.out.println("Please Give Orientation: ");
+
+        //Case that Orientation is Horizontal
+        if(or == 'h') {
+            for( int j=t.getX(); j<(t.getX()+size); j++){
+
+
+                b.board[t.getY()][j].setTileType(Tile.TileType.SHIP);
+
+
+            }
+
         }
+        //Case that Orientation is Vertical
+        else if( or== 'v' ) {
+            for(int i=t.getY(); i<(t.getY()+size); i++){
 
+                b.board[i][t.getX()].setTileType(Tile.TileType.SHIP);
+            }
+
+        }
     }
-    //Case that Orientation is Vertical
-     else if( or== 'v' ) {
-       for(int i=startCelly; i<size; i++){
-           
-         b.board[i][startCellx].setTileType(Tile.TileType.SHIP);
-       }
-     
-     }
-  }
 }
+
+
