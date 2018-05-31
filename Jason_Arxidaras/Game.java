4b1dbbfc954b3ef0d10a003e19d131ca87818fc3
funkyCEID <39421632+funkyCEID@users.noreply.github.com>
2018-05-31 22:32:20 +0300

    Update Game.java

--- Jason_Arxidaras/Game.java
+++ Jason_Arxidaras/Game.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 public class Game {
 
-    static int[] getInput(){
+     static int[] getInput(){
         int pin[]=new int[2];
         pin[0]=15;
         pin[1]=15;
 
-        Scanner read=new Scanner(System.in).useDelimiter("\\D*\\D");   // doesn`t read space
+        Scanner read=new Scanner(System.in).useDelimiter("\\D*\\D");   // doesn't read space
 
         while(pin[0]<0 || pin[0]>9 || pin[1]<0 || pin[1]>9) {
             System.out.println("Give two numbers divided by some space between 0 and 9 :");
@@ -16,7 +16,7 @@ public class Game {
             pin[1] = read.nextInt();
         }
         return pin;
-    }
+     }
 
     static int []getRandInput(){
         Random rand=new Random();
@@ -26,7 +26,7 @@ public class Game {
         return pin;
     }
 
-    static char getRandOrientation(){
+    public static char getRandOrientation(){
         Random rand=new Random();
         int r=rand.nextInt(2);
         if(r==0){
@@ -50,25 +50,33 @@ public class Game {
         return read;
     }
 
-    String getOrientation()throws IOException{
-        String s="";
+    static char getOrientation()throws IOException{
+        char s=' ';
         System.out.println("Give h or v for orientation ");
 
         char c=(char)System.in.read();
         if(c=='h'){
-            s="Orientation.horizontal";
+            s= 'h' ;
         }
         else if(c=='v'){
-            s="Orientation.vertical";
+            s= 'v' ;
         }
         return s;
     }
 
-    public static void main(String[] args){
+    /* ------ M A I N ------- M A I N ------ M A I N ------- M A I N ------ M A I N ------ M A I N ------- M A I N ------ M A I N ------- M A I N ------ M A I N ------ M A I N ------- M A I N -------- */
+    public static void main(String[] args)throws OverlapTilesException, OversizeException, AdjacentTilesException, IOException{
 
 
         //Scanners
         Scanner scname = new Scanner(System.in);
+
+        //Creating Ships
+        Battleship battleship = new Battleship();
+        Carrier carrier = new Carrier();
+        Cruiser cruiser = new Cruiser();
+        Destroyer destroyer = new Destroyer();
+        Submarine submarine = new Submarine();
 
         //Creating Player
         String name = scname.nextLine(); //Player gives his own name
@@ -77,9 +85,9 @@ public class Game {
         //Creating Bot
         Player bot = new Player("bot");
 
-        bot.placeAllShips(bot.getPlayerBoard());    //It has a Board as an arguement
+        bot.placeAllShips(bot.getPlayerBoard());
 
-        while(randomPlace()){
+        while(randomPlace()) {
 
             user.placeAllShips(user.getPlayerBoard());
             user.getPlayerBoard().drawboards();
@@ -88,18 +96,43 @@ public class Game {
         }
 
         int[] coordin = new int[2];
-        coordin=user.getInput();
 
-        Tile tileplaceship = new Tile(coordin[0] , coordin[1] , Tile.TileType.SHIP);
+        //Placement of Battleship
+        coordin = Game.getInput();
+        Tile tilePlaceBattleship = new Tile(coordin[0] , coordin[1] , Tile.TileType.SHIP);
+        char orientBattleship =  Game.getOrientation();
+        user.placeShip(battleship ,tilePlaceBattleship, orientBattleship , user.getPlayerBoard(),false);
 
-        user.placeShip(tileplaceship, user.getOrientation() , user.getPlayerBoard,false);
+        //Placement of Carrier
+        coordin = Game.getInput();
+        Tile tilePlaceCarrier = new Tile(coordin[0] , coordin[1] , Tile.TileType.SHIP);
+        char orientCarrier =  Game.getOrientation();
+        user.placeShip(carrier ,tilePlaceCarrier, orientCarrier , user.getPlayerBoard(),false);
+
+        //Placement of Cruiser
+        coordin = Game.getInput();
+        Tile tilePlaceCruiser = new Tile(coordin[0] , coordin[1] , Tile.TileType.SHIP);
+        char orientCruiser =  Game.getOrientation();
+        user.placeShip(cruiser ,tilePlaceCruiser, orientCruiser , user.getPlayerBoard(),false);
+
+        //Placement of Destroyer
+        coordin = Game.getInput();
+        Tile tilePlaceDestroyer = new Tile(coordin[0] , coordin[1] , Tile.TileType.SHIP);
+        char orientDestroyer =  Game.getOrientation();
+        user.placeShip(destroyer ,tilePlaceDestroyer, orientDestroyer , user.getPlayerBoard(),false);
+
+        //Placement of Submarine
+        coordin = Game.getInput();
+        Tile tilePlaceSubmarine = new Tile(coordin[0] , coordin[1] , Tile.TileType.SHIP);
+        char orientSubmarine =  Game.getOrientation();
+        user.placeShip(submarine ,tilePlaceSubmarine, orientSubmarine , user.getPlayerBoard(),false);
 
 
-        while(!user.getPlayerBoard().allShipsSunk() || bot.getPlayerBoard().allShipsSunk()){    //If either of them loses the game is over
-            coordin = user.getInput();
+        while(!user.getPlayerBoard().allShipsSunk() || !bot.getPlayerBoard().allShipsSunk()){    //If either of them loses the game is over
+            coordin = Game.getInput();
             user.fire(user.getPlayerBoard() , coordin);
 
-            coordin = bot.getRandInput();
+            coordin = Game.getRandInput();
             bot.fire(bot.getPlayerBoard() , coordin);
 
             user.getPlayerBoard().drawboards();
