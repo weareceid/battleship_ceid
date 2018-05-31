f1d3d06601d24a09ff2dc4654cad6aab29c0c44c
funkyCEID <39421632+funkyCEID@users.noreply.github.com>
2018-05-31 22:31:17 +0300

    Update Player.java

--- Jason_Arxidaras/Player.java
+++ Jason_Arxidaras/Player.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 public class Player {
     //Variables
-    private String playerName;
+    private String name;
     private int countShots;  //Counter of shots
     private int countMiss;   //Counter of missed shots
     private int countHit;    //Counter of successful hits
     private int countRep;    //Counter of repetitive shots
     private Board board = new Board();
 
-    public Player(String name){
-        playerName = name;
+    //Constructor
+    public Player(String name) {
+        this.name = name;
         this.countShots = 0;
         this.countMiss = 0;
         this.countHit = 0;
         this.countRep = 0;
     }
-    
-      //Getters
+    //Getters
     public String getName(){ return name; }
     public int getCountShots() { return countShots; }
     public int getCountHit () { return countHit; }
@@ -28,31 +28,35 @@ public class Player {
         b.placeAllShips(board);
     }
 
-    public void placeShip(Ship s, int Startx, int Starty) throws OverlapTilesException, OversizeException, AdjacentTilesException {       //Ship's method is called
-        Tile t = new Tile(Startx, Starty, Tile.TileType.SHIP);
-        s.placeShip(t, 'v',board,false);
+    public void placeShip(Ship s,Tile t,char or, Board b, boolean verbose) throws OverlapTilesException, OversizeException, AdjacentTilesException {       //Ship's method is called
+        s.placeShip(t, or, b, verbose);
     }
 
 
     public void getStats(){
-        System.out.println("Number of shots: " + countShots + "\tNumber of missed shots: " + countMiss +
+        System.out.println("Number of shots: " + countHit + "\tNumber of missed shots: " + countMiss +
                 "\tNumber of hits: " + countHit + "\tNumber of repetitive shots: " + countRep);
     }
 
     public void fire(Board b, int pin[]){
         countShots++;
         if(b.board[pin[0]][pin[1]].getSymbol() == 's'){ 	//If the attacked tile is SHIP tile it changes its type to HIT and adds to the counter
-                b.board[pin[0]][pin[1]].setTileType(Tile.TileType.HIT);
-                System.out.println("Hit!");
-                countHit++;
-            }else if(b.board[pin[0]][pin[1]].getSymbol() == '~'){  //If the attacked tile is SEA tile it changes its type to MISS and adds to the counter
-                b.board[pin[0]][pin[1]].setTileType(Tile.TileType.MISS);
-                System.out.println("Miss!");
-                countMiss++;
-            }else if(b.board[pin[0]][pin[1]].getSymbol() == 'x' || b.board[pin[0]][pin[1]].getSymbol() == 'o'){     //If the attacked tile is HIT or MISS tile it adds to the counter
-                System.out.println("Already hit the spot!");
-                countRep++;
-            }
+            b.board[pin[0]][pin[1]].setTileType(Tile.TileType.HIT);
+            System.out.println("Hit!");
+            countHit++;
+        }else if(b.board[pin[0]][pin[1]].getSymbol() == '~'){  //If the attacked tile is SEA tile it changes its type to MISS and adds to the counter
+            b.board[pin[0]][pin[1]].setTileType(Tile.TileType.MISS);
+            System.out.println("Miss!");
+            countMiss++;
+        }else if(b.board[pin[0]][pin[1]].getSymbol() == 'x' || b.board[pin[0]][pin[1]].getSymbol() == 'o'){     //If the attacked tile is HIT or MISS tile it adds to the counter
+            System.out.println("Already hit the spot!");
+            countRep++;
         }
+    }
+
+    public Board getPlayerBoard(){
+        return board;
+    }
 }
+
 
